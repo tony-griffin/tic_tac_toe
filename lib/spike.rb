21f83a26ab36cabbfdddc114e9321797516fcdc1
Ninamma Rai <ninammarai@Ninammas-MacBook-Pro.local>
2019-09-25 14:10:59 +0100

    Got a working class MakeTree that we spiked that works with minimax_too, AI's still beatable

--- lib/spike.rb
+++ lib/spike.rb
@@ -8,7 +8,8 @@ require "./minimax_too.rb"
 
 gateway = InMemoryGameGateway.new
 game = Game.new
-game.board = ['O','X','X','O','O','X','X','','']
+# game.board = ['O','X','X','O','O','X','X','','']
+# game.board = ['X','','O','O','','X','X','','']
 gateway.save_game(game)
 display_board = DisplayBoard.new(gateway) 
 puts display_board.execute({})
@@ -17,20 +18,8 @@ check_status = CheckGameStatus.new(gateway)
 
 class MakeTree
 
-    # def initialize(gateway)
-    #     @gateway = gateway
-    #     @game = gateway.game
-    #     @board = gateway.game.board
-    # end
-
-    def self.get_tree(board)
-        tree = []
-        empty_square_array = get_empty_squares(board)
-        
-
-    end
-
     def self.make_tree_from_board(board)
+        full_tree = []
         empty_square_array = get_empty_squares(board)
         empty_square_array.each do |empty_square|
             node = {position: empty_square, children: [] ,score: 0}
@@ -48,10 +37,12 @@ class MakeTree
                 node[:score] = 10
             elsif check_win_status.has_won? && !check_win_status.last_player_ai?
                 node[:score] = -10
+            else
+                node[:children] = make_tree_from_board(fake_game.board) # Check with Craig?? :D
             end
-            node[:children] = make_tree_from_board(fake_game.board)
-            return node
+            full_tree << node
         end
+        full_tree
     end
 
     def self.get_empty_squares(board)
@@ -59,25 +50,29 @@ class MakeTree
     end
 
 end
-tree = [{position: 2, children: [{position: 3, children: [], score: -10},
-                                         {position: 5, children: [{position: 9, children: [], score: 10},
-                                                                  {position: 8, children: [], score: 0}], score: 0}], score: 0},
-                {position: 3, children: [], score: 0}]
 
-pp Minimax.best_move(tree)
-pp MakeTree.make_tree_from_board(gateway.game.board)
+# tree = MakeTree.make_tree_from_board(gateway.game.board)
+# pp tree
+# pp Minimax.best_move(tree)
 
-        
-# while check_status.over? == false do
-#     cli_input = GetCliInput.new(gateway)
-#     player_choice = EnterChoice.new(cli_input.execute, gateway)
-#     player_choice.mark_square
-#     display_board = DisplayBoard.new(gateway) 
-#     puts display_board.execute({})
-# end
+def get_ai_choice(gateway)
+    tree = MakeTree.make_tree_from_board(gateway.game.board)
+    Minimax.best_move(tree)
+end
 
-# if check_status.has_won?
-#     puts "YOU WON!"
-# elsif check_status.draw?
-#     "IT'S A DRAW!"
-# end
+while check_status.over? == false do
+    cli_input = GetCliInput.new(gateway)
+    player_choice = EnterChoice.new(cli_input.execute, gateway)
+    player_choice.mark_square
+    ai_input = get_ai_choice(gateway)
+    ai_choice = EnterChoice.new(ai_input + 1, gateway)
+    ai_choice.mark_square
+    display_board = DisplayBoard.new(gateway) 
+    puts display_board.execute({})
+end
+
+if check_status.has_won?
+    puts "YOU WON!"
+elsif check_status.draw?
+    "IT'S A DRAW!"
+end
